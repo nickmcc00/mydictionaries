f33c233878af23ece1bc3ad3974fadf2e9e57373
nickmcc00 <nicholas_mccullough1@baylor.edu>
2023-02-06 15:40:25 -0600

    worked problems

--- 7_schools_report.py
+++ 7_schools_report.py
@@ -11,9 +11,35 @@ Copy that info here:
 "NCAA/NAIA conference number football (IC2020)","130","Southeastern Conference"
 
 
-Display report for all universities that have a graduation rate for Women over 50%
+Display report for all universities that have a graduation rate for Women over 80%
 Display report for all universities that have a total price for in-state students living off campus over $50,000
 
 
 
 """
+
+import json
+
+infile = open("school_data.json", "r")
+
+schools = json.load(infile)
+# can make list for what numbers are looked for
+conference_schools = [372, 108, 107, 130]
+
+print(type(schools))
+
+# How many schools are in this file?
+print(len(schools))
+
+for school in schools:
+    if school["NCAA"]["NAIA conference number football (IC2020)"] in conference_schools:
+        if school["Graduation rate  women (DRVGR2020)"] > 80:
+            print(f"University Name: {school['instnm']}")
+            print(
+                f"Graduation Rate for Women: {school['Graduation rate  women (DRVGR2020)']}"
+            )
+            print()
+            print()
+
+
+# Instate students living off campus over $50,000
